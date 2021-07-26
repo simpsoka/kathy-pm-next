f6fb34d7a9991dabcc1448bd85f618c873651c5f
Kathy Korevec <simpsoka@github.com>
2021-07-26 07:39:59 -0700

    Update layout.js
    Added open graph meta tags

--- components/layout.js
+++ components/layout.js
@@ -42,6 +42,9 @@ export default function Layout({ children }) {
         <meta name="viewport" content="user-scalable=0, initial-scale=1.0" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta property="og:title" content="Kathy Korevec: Product Manager" />
+        <meta property="og:image" content="https://kathy.pm/images/avatar-compressed.webp"  />
+
         <title>kathy.pm</title>
         <link rel="icon" href="/favicon.ico" />
         <link
