dc848f7ea713f3e219a2d8737875f6936bc752b8
Kathy Korevec <simpsoka@github.com>
2021-07-25 20:09:16 -0700

    Update philosophy.js
    Fixed background color

--- pages/philosophy.js
+++ pages/philosophy.js
@@ -2,7 +2,7 @@ import Markdown from '../data/philosophy.mdx';
 
 export default function Philosophy() {
   return (
-    <article className="prose md:prose-xl dark:prose-dark">
+    <article className="prose md:prose-xl dark:prose-dark dark:bg-gray-800 bg-white">
       <Markdown />
     </article>
   );
