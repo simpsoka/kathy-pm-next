77344425c8c56c2b6c59426ca663a444142f6579
Kathy Korevec <simpsoka@github.com>
2021-07-26 07:59:54 -0700

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -42,7 +42,7 @@ export default function Layout({ children }) {
         <meta name="viewport" content="user-scalable=0, initial-scale=1.0" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <meta property="og:title" content="Kathy Korevec: Product Manager" />
+        <meta property="og:title" content="Kathy Korevec" />
         <meta property="og:description" content="Kathy Korevec: Head of Product at Vercel"  />
         <meta property="og:image" content="https://kathy.pm/images/avatar-compressed.webp"  />
 
