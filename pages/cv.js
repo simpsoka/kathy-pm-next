cc10e93c9ec1500992864119e68324fbaa7cd957
Kathy Korevec <simpsoka@github.com>
2021-07-25 20:09:51 -0700

    Update cv.js
    Fixed background color with pattern

--- pages/cv.js
+++ pages/cv.js
@@ -2,7 +2,7 @@ import Markdown from '../data/cv.mdx';
 
 export default function Resume() {
   return (
-    <article className="prose md:prose-xl dark:prose-dark">
+    <article className="prose md:prose-xl dark:prose-dark dark:bg-gray-800 bg-white">
       <Markdown />
     </article>
   );
