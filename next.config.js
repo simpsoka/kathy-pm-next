e63cfc895074542feb827d7453fb130a60661b2f
Kathy Korevec <simpsoka@gmail.com>
2021-07-26 16:52:24 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
+const fs = require('fs');
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 });
 
 module.exports = withMDX({
+  env: {
+   noflash: fs.readFileSync('./public/scripts/noflash.js').toString()
+ },
   i18n: {
     locales: ['en-US'],
     defaultLocale: 'en-US',
