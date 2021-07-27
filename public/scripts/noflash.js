6ece2cbeb91898e9e5ca733a5a261810841a3ce3
Kathy Korevec <simpsoka@gmail.com>
2021-07-26 17:24:38 -0700

    Update noflash.js

--- public/scripts/noflash.js
+++ public/scripts/noflash.js
@@ -3,12 +3,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const w = d.defaultView;
     w.localStorage.theme = 'dark'
     document.body.classList.add('dark');
+    console.log('dark on');
   }
 
   const toggleDarkOff = (d) => {
     const w = d.defaultView;
     w.localStorage.theme = 'default'
     document.body.classList.remove('dark');
+    console.log('dark off');
   }
 
   if (window.localStorage.theme === 'dark' ||
