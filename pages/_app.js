4bac5bf160c84ed4ac9bc08b9a52ada23e4f4aba
Kathy Korevec <simpsoka@gmail.com>
2022-10-20 10:16:25 -0700

    Update _app.js
    analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import 'tailwindcss/tailwind.css';
 import './styles.css'
+import Script from 'next/script';
 
 import Layout from '../components/layout';
 
@@ -7,6 +8,9 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      {process.env.NODE_ENV === 'production' ? (
+            <Script id="insights" src="/va/script.js" strategy="afterInteractive" />
+          ) : null}
     </Layout>
   );
 }
